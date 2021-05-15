6692db9fa367a7e1c7f318cf637afcb36a6d6ec9
Andreas Janzen <janzen@gmx.net>
2021-05-15 22:33:34 +0200

    Added a README.md and a first attempt to get the absolute paths to the pdf files recursively.

---
+++ README.md
@@ -0,0 +1,19 @@
+# pdf\_scraper
+
+**pdf\_ scraper** is a tool to find all pdf files in a folder and all of its
+sub-folders, extract the text from each pdf files, remove punctuation and
+stop-words from the text, and count the number of word occurrences in the text.
+The most common keywords are stored for each pdf file together with the file
+path. In the end, similar pdf files can be identified by comparison of the
+keywords between files.
+
+```./pdf_scraper <path>
+```
+
+## Installing pdf\_scraper
+
+pdf\_scraper is not yet available on PyPI, but I will do my best.
+
+```console
+$ python -m pip install pdf_scraper
+```
--- pdf_scraper.py
+++ pdf_scraper.py
@@ -18,15 +18,65 @@ so that -- finally -- pdf files with similar content can be identified
 automatically.
 
 The motivation for this app is the vast number of scientific publications that
-I have gathered for work and which have now become too many to sort them by
+I have gathered at work and which have now become too many to sort them by
 hand.
 """
+
+import os
+import PyPDF2
+import sys
+
+from collections import Counter
+from string import ascii_letters, digits, punctuation
+
+
+STOP_WORDS_ENGLISH = "./stop_words/stop_words_english.txt"
+STOP_WORDS_GERMAN  = "./stop_words/stop_words_german.txt"
+
+
+def get_pdf_filenames(directory):
+    """ TO DO: write docstring
+    """
+    files = list()
+    dirlist = [directory]
+
+    while len(dirlist) > 0:
+        for (dirpath, dirnames, filenames) in os.walk(dirlist.pop()):
+            dirlist.extend(dirnames)
+            files.extend(map(lambda n: os.path.join(*n), zip([dirpath] *
+                len(filenames), filenames)))
+
+    pdf_files = [file for file in files if file.endswith(".pdf")]
+
+    print("Found the following pdf files:\n")
+    for file in pdf_files:
+        print(f"\t{file}")
+
+    return pdf_files
+
+
+def get_keywords(text):
+    """ TO DO: write docstring
+    """
+    return keywords
+
+
+def write_keyword_table():
+    """ TO DO: write docstring
+    """
+    pass
 
 
 # Program starts here
 def main():
-    pass
+    if len(sys.argv) == 2:
+        directory = sys.argv[1]
+        get_pdf_filenames(directory)
+    else:
+        print("\nUSAGE: pdf_scraper <path>")
+
+    sys.exit()
+
 
 if __name__ == "__main__":
     main()
-
