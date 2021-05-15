a8e43aa74aae9ef608c7c8bdcafed79b4ab6a570
Andreas Janzen <janzen@gmx.net>
2021-05-15 20:48:08 +0200

    Project kickoff, first commit

---
+++ pdf_scraper.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""
+==============
+pdf_scraper.py
+==============
+
+Author : Dr. Andreas Janzen
+Email  : janzen (at) gmx (dot) net
+Date   : 2021-05-15
+Version: V0.1
+
+Scrape all pdf documents in the current folder and all sub-folders, extract the
+text from these files, remove all punctuation and numbers, and then count the
+remaining words in the text to identify the most frequently used characteristic
+words in the document. These words will be stored (e.g. in an SQLite database)
+so that -- finally -- pdf files with similar content can be identified
+automatically.
+
+The motivation for this app is the vast number of scientific publications that
+I have gathered for work and which have now become too many to sort them by
+hand.
+"""
+
+
+# Program starts here
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
